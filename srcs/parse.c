e3f48488af0b39d0bbee43ceb38dba3c59ea13ee
gpffh20 <eunbin584836@gmail.com>
2024-03-11 02:37:44 +0900

    fix: fdf->cub로 변경

--- srcs/parse.c
+++ srcs/parse.c
@@ -48,22 +48,6 @@ int	get_path(char *line, t_game_info *game)
 	return (SUCCESS);
 }
 
-void	invalid_file(char *file_name)
-{
-	int		len;
-	char	*tmp;
-
-	len = ft_strlen(file_name);
-	tmp = ft_substr(file_name, len - 4, 4);
-	if (ft_strncmp(tmp, ".fdf", 4))
-	{
-		free(tmp);
-		return ;
-	}
-	free(tmp);
-	error_exit("Error: map extension error.");
-}
-
 void	get_map(char *map_file, t_game_info *game)
 {
 	int		fd;
@@ -93,4 +77,20 @@ void	get_map(char *map_file, t_game_info *game)
 	map = ft_split(total_line, '\n');
 	free(total_line);
 	game->map = map;
+}
+
+void	invalid_file(char *file_name)
+{
+	int		len;
+	char	*tmp;
+
+	len = ft_strlen(file_name);
+	tmp = ft_substr(file_name, len - 4, 4);
+	if (ft_strncmp(tmp, ".cub", 4))
+	{
+		free(tmp);
+		return ;
+	}
+	free(tmp);
+	error_exit("Error: map extension error.");
 }
