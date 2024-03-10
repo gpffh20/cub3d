76ae827e36f8ab93b5b1b88eb4c9be71abaa6066
gykoh42 <karmakoh42@naver.com>
2024-03-11 00:55:34 +0900

    feat: header define 및 함수 추가

--- include/cub3D.h
+++ include/cub3D.h
@@ -1,9 +1,34 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+# define FAIL -1
+# define SUCCESS 0
+
+# define CEILING 1
+# define FLOOR 2
+
 # include <stdlib.h>
+# include <fcntl.h>
 
 // test header
 # include <stdio.h>
+typedef struct s_game_info
+{
+	char	*no_path;
+	char	*so_path;
+	char	*we_path;
+	char	*ea_path;
+	int		*floor_color;
+	int		*ceiling_color;
+	int		start_x;
+	int		start_y;
+	char	**map;
+	// char	*sprite_texture;
+}				t_game_info;
+
+error_exit(char *str);
+void	invalid_file(char *file_name);
+
+
 
 #endif
