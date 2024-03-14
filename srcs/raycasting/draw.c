00a37dfde1c96d5362931f24821dcb2b279a418e
gykoh42 <karmakoh42@naver.com>
2024-03-14 21:53:36 +0900

    feat: 천장, 바닥 색상 입히기 완료

---
+++ srcs/raycasting/draw.c
@@ -0,0 +1,33 @@
+#include "../../include/cub3D.h"
+
+void	paint_background(t_game_info *game)
+{
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < SCREEN_HEIGHT / 2)
+	{
+		x = 0;
+		while (x < SCREEN_WIDTH)
+		{
+			mlx_pixel_put(game->mlx, game->win, x, y, game->floor_color);
+			x++;
+		}
+		y++;
+	}
+	while (y < SCREEN_HEIGHT)
+	{
+		x = 0;
+		while (x < SCREEN_WIDTH)
+		{
+			mlx_pixel_put(game->mlx, game->win, x, y, game->ceiling_color);
+			x++;
+		}
+		y++;
+	}
+}
+// void	draw_map(t_game_info *game)
+// {
+
+// }
