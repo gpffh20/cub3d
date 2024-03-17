613ec74b570aa1a552b7bc2c0d1c4ab7b9e8f20b
gykoh42 <karmakoh42@naver.com>
2024-03-17 23:58:20 +0900

    feat: unit vector들 init 하는 로직 추가 및 dda 알고리즘 구현 (테스트 필요함)

--- srcs/raycasting/draw.c
+++ srcs/raycasting/draw.c
@@ -28,6 +28,54 @@ void	paint_background(t_game_info *game)
 	}
 }
 
+void	init_vectors(t_raycast *ray, t_game_info *game)
+{
+	if (ray->ray_dir.x < 0)
+	{
+		ray->step_dir.x = -1;
+		ray->side_dist.x = (game->ray.player.x - ray->player_pos.x) * ray->delta_dist.x;
+	}
+	else
+	{
+		ray->step_dir.x = 1;
+		ray->side_dist.x = (ray->player_pos.x + 1.0 - game->ray.player.x) * ray->delta_dist.x;
+	}
+	if (ray->ray_dir.y < 0)
+	{
+		ray->step_dir.y = -1;
+		ray->side_dist.y = (game->ray.player.y - ray->player_pos.y) * ray->delta_dist.y;
+	}
+	else
+	{
+		ray->step_dir.y = 1;
+		ray->side_dist.y = (ray->player_pos.y + 1.0 - game->ray.player.y) * ray->delta_dist.y;
+	}
+}
+
+// void	dda(t_game_info *game, t_raycast *ray)
+// {
+// 	int hit_flag;
+
+// 	hit_flag = 0;
+// 	while (hit_flag == 0)
+// 	{
+// 		if (ray->side_dist.x < ray->side_dist.y)
+// 		{
+// 			ray->side_dist.x += ray->delta_dist.x;
+// 			ray->player_pos.x += ray->step_dir.x;
+// 			ray->side = 0;
+// 		}
+// 		else
+// 		{
+// 			ray->side_dist.y += ray->delta_dist.y;
+// 			ray->player_pos.y += ray->step_dir.y;
+// 			ray->side = 1;
+// 		}
+// 		if (game->map[ray->player_pos.y][ray->player_pos.x] == '1')
+// 			hit_flag = 1;
+// 	}
+// }
+
 void	draw_map(t_game_info *game)
 {
 	int monitor;
@@ -37,6 +85,7 @@ void	draw_map(t_game_info *game)
 	while (monitor < SCREEN_WIDTH)
 	{
 		calc_ray_params(game, &game->ray, monitor);
+		init_vectors(&game->ray, game);
 		// TODO: dda 알고리즘 구현
 		monitor++;
 	}
