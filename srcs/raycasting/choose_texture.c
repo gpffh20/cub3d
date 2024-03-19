cbefd73c213b54040793139f774e62015f02f3d9
gpffh20 <eunbin584836@gmail.com>
2024-03-19 22:07:16 +0900

    rename: raycasting 파일 분리

---
+++ srcs/raycasting/choose_texture.c
@@ -0,0 +1,13 @@
+#include "../../include/cub3D.h"
+
+void choose_texture(t_game_info *game, t_raycast *ray)
+{
+	if (ray->side == VER_LINE && ray->ray_dir.y > 0)
+		ray->wall_type = &game->so_texture;
+	else if (ray->side == VER_LINE && ray->ray_dir.y < 0)
+		ray->wall_type = &game->no_texture;
+	else if (ray->side == HOR_LINE && ray->ray_dir.x > 0)
+		ray->wall_type = &game->we_texture;
+	else if (ray->side == HOR_LINE && ray->ray_dir.x < 0)
+		ray->wall_type = &game->ea_texture;
+}
